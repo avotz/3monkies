bb26fdd925cc628c544632a40ad3cf159f96795e
Alonso Avotz <alonso@avotz.com>
2018-05-23 17:32:53 -0600

    se actualizo woocomerce

--- wp-content/themes/threemonkies/woocommerce/archive-product.php
+++ wp-content/themes/threemonkies/woocommerce/archive-product.php
@@ -10,43 +10,39 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates
+ * @version 3.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 get_header( 'shop' );
 
 ?>
-
 <div class="main">
 
-          <div class="banner">
-              <!-- <div class="banner-container">
-                <div class="banner-info">
-                  
-                  <h1 class="animated fadeIn">Experience Costa Rica with</h1>
-                  <p>Local Specialists</p>
-                </div>
-                
-              </div> 
-               -->
-          </div>
-          <div class="home-container">
-            <div class="home-info">
-                    
-                    <h1 class="animated fadeIn">Experience Costa Rica with</h1>
-                    <p>Local Specialists</p>
-            </div>
-           </div> 
+	<div class="banner">
+		<!-- <div class="banner-container">
+		<div class="banner-info">
+			
+			<h1 class="animated fadeIn">Experience Costa Rica with</h1>
+			<p>Local Specialists</p>
+		</div>
+		
+		</div> 
+		-->
+	</div>
+	<div class="home-container">
+	<div class="home-info">
+			
+			<h1 class="animated fadeIn">Experience Costa Rica with</h1>
+			<p>Local Specialists</p>
+	</div>
+	</div> 
 
-         
-       </div>
+	
+</div>
+
 <?php
-get_footer('shop');
-?>
+get_footer( 'shop' ); ?>
