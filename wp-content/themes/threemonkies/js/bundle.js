c6f48bd24f0923cebfec858ae12478cb5b51776f
Alonso Avotz <alonso@avotz.com>
2018-05-30 11:16:55 -0600

    test service workrer

--- wp-content/themes/threemonkies/js/bundle.js
+++ wp-content/themes/threemonkies/js/bundle.js
@@ -2410,6 +2410,18 @@ function getWindowWidth() {
 
 
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw-monkiescr.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
 ;(function($){
   
 
