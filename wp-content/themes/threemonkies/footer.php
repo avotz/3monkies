9fa1389a6121b7320b80da7f01f4822d5e34d839
Alonso Chavarria <alonso@avotz.com>
2019-11-12 15:22:54 -0600

    se agrego membre a demas

--- wp-content/themes/threemonkies/footer.php
+++ wp-content/themes/threemonkies/footer.php
@@ -45,22 +45,22 @@
 	</div>
 
 	<div id="tour-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
-         <h2>Reserve Tours</h2>
+         <h2>Reserve Tours <img src="<?php echo get_template_directory_uri(); ?>/img/logo-faces.png" alt="3 Monkies Tours" style="position: absolute;right: 35px;top: 5px;"></h2>
          <?php echo do_shortcode('[contact-form-7 id="32" title="Book Tour"]'); ?>
-              
+		 <div class="membrete"></div>    
         
     </div>
 	<div id="transportation-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
-         <h2>Reserve Transportation <img src="<?php echo get_template_directory_uri(); ?>/img/logo-faces.png" alt="3 Monkies Tours"></h2>
+         <h2>Reserve Transportation <img src="<?php echo get_template_directory_uri(); ?>/img/logo-faces.png" alt="3 Monkies Tours" style="position: absolute;right: 35px;top: 5px;"></h2>
 		 <?php echo do_shortcode('[contact-form-7 id="33" title="Book Transportation"]'); ?>
 		 <div class="membrete"></div>
               
         
     </div>
 	<div id="contact-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
-         <h2>Contact Us</h2>
+         <h2>Contact Us <img src="<?php echo get_template_directory_uri(); ?>/img/logo-faces.png" alt="3 Monkies Tours" style="position: absolute;right: 35px;top: 5px;"></h2>
          <?php echo do_shortcode('[contact-form-7 id="31" title="Contact form"]'); ?>
-              
+		 <div class="membrete"></div>
         
     </div>
 	
