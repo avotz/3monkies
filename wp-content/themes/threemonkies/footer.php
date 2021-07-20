3f7619a3d5db3af85e7f89e6ace5a8a7480f3ef3
Alonso Chavarria <alonso@avotz.com>
2021-07-20 11:02:40 -0600

    testing

--- wp-content/themes/threemonkies/footer.php
+++ wp-content/themes/threemonkies/footer.php
@@ -49,6 +49,15 @@
 	<h2>Reserve Tours <img src="<?php echo get_template_directory_uri(); ?>/img/logo-faces.png" alt="3 Monkies Tours" style="position: absolute;right: 35px;top: 5px;"></h2>
 	<?php echo do_shortcode('[contact-form-7 id="32" title="Book Tour"]'); ?>
 	<div class="membrete"></div>
+	<div id="smart-button-container">
+		<!-- <div style="text-align: center"><label for="description"> </label><input type="text" name="descriptionInput" id="description" maxlength="127" value=""></div>
+		<p id="descriptionError" style="visibility: hidden; color:red; text-align: center;">Please enter a description</p> -->
+		<div style="text-align: center"><label for="amount">Amount </label><input name="amountInput" type="number" id="amount" value=""><span> USD</span></div>
+		<p id="priceLabelError" style="visibility: hidden; color:red; text-align: center;">Please enter a price</p>
+		<div id="invoiceidDiv" style="text-align: center; display: none;"><label for="invoiceid"> </label><input name="invoiceid" maxlength="127" type="text" id="invoiceid" value=""></div>
+		<p id="invoiceidError" style="visibility: hidden; color:red; text-align: center;">Please enter an Invoice ID</p>
+		<div style="text-align: center; margin-top: 0.625rem;" id="paypal-button-container"></div>
+	</div>
 
 </div>
 <div id="transportation-popup" class="request-popup white-popup mfp-hide mfp-with-anim">
@@ -66,19 +75,11 @@
 </div>
 
 
-<div id="smart-button-container">
-		<div style="text-align: center"><label for="description"> </label><input type="text" name="descriptionInput" id="description" maxlength="127" value=""></div>
-		<p id="descriptionError" style="visibility: hidden; color:red; text-align: center;">Please enter a description</p>
-		<div style="text-align: center"><label for="amount"> </label><input name="amountInput" type="number" id="amount" value=""><span> USD</span></div>
-		<p id="priceLabelError" style="visibility: hidden; color:red; text-align: center;">Please enter a price</p>
-		<div id="invoiceidDiv" style="text-align: center; display: none;"><label for="invoiceid"> </label><input name="invoiceid" maxlength="127" type="text" id="invoiceid" value=""></div>
-		<p id="invoiceidError" style="visibility: hidden; color:red; text-align: center;">Please enter an Invoice ID</p>
-		<div style="text-align: center; margin-top: 0.625rem;" id="paypal-button-container"></div>
-	</div>
+
 	<script src="https://www.paypal.com/sdk/js?client-id=AeHyejoa9M1udO7Qk1AznenkuM0fXKSD9iPWGTjyZJP_PFxj24PBKZZ9gDXg_2smtsaZLhPLkBAwZ5lb&enable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
 	<script>
 		function initPayPalButton() {
-			var description = document.querySelector('#smart-button-container #description');
+			var description = document.querySelector('.wpcf7-form-control-wrap tours select[name="tours"]');
 			var amount = document.querySelector('#smart-button-container #amount');
 			var descriptionError = document.querySelector('#smart-button-container #descriptionError');
 			var priceError = document.querySelector('#smart-button-container #priceLabelError');
