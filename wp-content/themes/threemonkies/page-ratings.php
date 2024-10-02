1e53a0370d8662b6789d2ee073b870fc2d48d905
Alonso Chavarria <alons182@hotmail.com>
2024-10-01 19:41:53 -0600

    Add template for page ratings

---
+++ wp-content/themes/threemonkies/page-ratings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * Template Name: Page Ratings
+ * @package threemonkies
+ */
+
+//get_header();
+?>
+
+	<div class="main">
+		
+		
+	</div><!-- #main -->
+
+
+<?php
+//get_sidebar();
+//get_footer();
